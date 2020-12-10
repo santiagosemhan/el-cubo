9100c494430ef55e900fcc72ba88e84168e6c0b9
jositux <jm.guaimas@gmail.com>
2020-12-10 20:02:50 -0300

    Agregado wizard help

---
+++ website/js/help.js
@@ -0,0 +1,115 @@
+const openModalTriggerEl = document.querySelector('.open_modal');
+const closeModalTriggerEl = document.querySelector('.close_modal');
+const modalEl = document.querySelector('.modal');
+let currentTab = 0;
+
+function modal() {
+    openModalTriggerEl.addEventListener('click', () => {
+        modalEl.classList.add('open');
+        currentTab = 0;
+    });
+    closeModalTriggerEl.addEventListener('click', () => {
+        modalEl.classList.remove('open');
+    });
+    window.addEventListener('click', () => {
+        if (event.target === modalEl) {
+            modalEl.classList.remove('open');
+        }
+    })
+}
+
+modal();
+
+
+
+/* Wizard */
+const previousButton = document.getElementById("previous")
+const nextButton = document.getElementById("next")
+const submitButton = document.getElementById('validate')
+const dots = document.getElementsByClassName('progress-bar__dot')
+const numberOfSteps = 3
+let currentStep = 1
+
+for (let i = 0; i < dots.length; ++i) {
+    dots[i].addEventListener('click', () => {
+        goToStep(i + 1)
+    })
+}
+
+previousButton.onclick = goPrevious
+nextButton.onclick = goNext
+
+
+function goNext(e) {
+    e.preventDefault()
+    currentStep += 1
+    goToStep(currentStep)
+}
+
+function goPrevious(e) {
+    e.preventDefault()
+    currentStep -= 1
+    goToStep(currentStep)
+}
+
+function goToStep(stepNumber) {
+    currentStep = stepNumber
+
+    let inputsToHide = document.getElementsByClassName('step')
+    let inputs = document.getElementsByClassName(`step${currentStep}`)
+    let indicators = document.getElementsByClassName('progress-bar__dot')
+
+    for (let i = indicators.length - 1; i >= currentStep; --i) {
+        indicators[i].classList.remove('full')
+    }
+
+    for (let i = 0; i < currentStep; ++i) {
+        indicators[i].classList.add('full')
+    }
+
+    //hide all input
+    for (let i = 0; i < inputsToHide.length; ++i) {
+        hide(inputsToHide[i])
+    }
+
+    //only show the right one
+    for (let i = 0; i < inputs.length; ++i) {
+        show(inputs[i])
+    }
+
+    //if we reached final step
+    if (currentStep === numberOfSteps) {
+        enable(previousButton)
+        disable(nextButton)
+        show(submitButton)
+    }
+
+    //else if first step
+    else if (currentStep === 1) {
+        disable(previousButton)
+        enable(next)
+        hide(submitButton)
+    } else {
+        enable(previousButton)
+        enable(next)
+        hide(submitButton)
+    }
+}
+
+function enable(elem) {
+    elem.classList.remove("disabled");
+    elem.disabled = false;
+}
+
+function disable(elem) {
+    elem.classList.add("disabled");
+    elem.disabled = true;
+}
+
+function show(elem) {
+    elem.classList.remove('hidden')
+}
+
+function hide(elem) {
+    elem.classList.add('hidden')
+}
