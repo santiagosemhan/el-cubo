397696575bae8805e27e2cc9717e2daad5a7e629
jositux <jm.guaimas@gmail.com>
2021-01-15 10:34:43 -0300

    Add reflexivo PMV

--- website/js/onboard.js
+++ website/js/onboard.js
@@ -17,4 +17,45 @@ function show() {
     } else {
         clearInterval(intervalID);
     }
+
+    loadPlayer();
+}
+
+
+function loadPlayer() {
+    let source = 'https://rtvcplay-media-content.s3.amazonaws.com/vod-content/424404/424404.m3u8';
+    const video = document.querySelector('video');
+
+    const player = new Plyr(video, {
+
+    });
+
+    if (!Hls.isSupported()) {
+        video.src = source;
+    } else {
+
+        const hls = new Hls();
+        hls.loadSource(source);
+        hls.attachMedia(video);
+        window.hls = hls;
+
+
+
+        player.on('ready', function () {
+
+
+        });
+
+        player.on('ended', function () {
+            console.log('end');
+            //document.getElementsByClassName('pane-video')[0].classList.add('hide');
+            //document.getElementsByClassName('hero-onboarding')[0].classList.remove('hide'); 
+        });
+
+
+
+    }
+
+    window.player = player;
+
 }
