70b2cdb08a10d61d21c661af64b6e189eb5e04a0
jositux <jm.guaimas@gmail.com>
2021-02-04 15:31:05 -0300

    Update PMV laberinto

--- public/laberinto/js/laberinto.js
+++ public/laberinto/js/laberinto.js
@@ -7,9 +7,6 @@ console.log('video: ' + video.dataset.video);
 
 
 window.onload = function () {
-    disableScroll();
-    console.log('video: ' + video.dataset.video);
-
     loadPlayer(video.dataset.video, video.dataset.poster);
     window.player.play();
 };
@@ -90,15 +87,6 @@ function loadPlayer(sURL, sPoster) {
         player.on('ended', function () {
 
             pane.classList.toggle('open');
-            // Add selected to child
-            document.getElementsByClassName(pane.dataset.relation)[0].classList.add('selected');
-
-
-
-            if (viewedAll()) {
-                document.getElementsByClassName('row-second')[0].classList.add('visible');
-                document.getElementsByClassName('characters')[0].classList.add('is-viewed');
-            }
 
             player.fullscreen.exit();
 
@@ -112,170 +100,6 @@ function loadPlayer(sURL, sPoster) {
 }
 
 
-// Check si ya fueron visto los 3
-function viewedAll() {
-    let all_videos = document.querySelectorAll('.toggle').length;
-    let viewed_videos = document.querySelectorAll('.toggle.selected').length;
-
-    if (all_videos === viewed_videos) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-
-// Enable Disable Scroll
-
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-let keys = {
-    37: 1,
-    38: 1,
-    39: 1,
-    40: 1
-};
-
-function preventDefault(e) {
-    e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
-}
-
-// modern Chrome requires { passive: false } when adding event
-let supportsPassive = false;
-try {
-    window.addEventListener(
-        'test',
-        null,
-        Object.defineProperty({}, 'passive', {
-            get: function () {
-                supportsPassive = true;
-            },
-        }),
-    );
-} catch (e) {}
-
-let wheelOpt = supportsPassive ? {
-    passive: false
-} : false;
-let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-// call this to Disable
-function disableScroll() {
-    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-    window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-    window.removeEventListener('touchmove', preventDefault, wheelOpt);
-    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-
-
-/* Modal Onboarding */
-
-const openModalTriggerEl = document.querySelector('.open-modal');
-const closeModalTriggerEl = document.querySelector('.close-modal');
-const modalEl = document.querySelector('.modal');
-
-/* Fake cover */
-//const fake_cover = document.querySelector('.fake-cover');
-
-
-function modal() {
-    if (openModalTriggerEl) {
-        openModalTriggerEl.addEventListener('click', () => {
-            modalEl.classList.add('open');
-            console.log(openModalTriggerEl);
-        });
-    }
-    if (closeModalTriggerEl) {
-        closeModalTriggerEl.addEventListener('click', () => {
-            modalEl.classList.remove('open');
-            //fake_cover.classList.add('visible');
-
-        });
-    }
-    window.addEventListener('click', () => {
-        if (event.target === modalEl) {
-            modalEl.classList.remove('open');
-        }
-    })
-
-    function keyPress(e) {
-        if (e.key === "Escape") {
-            modalEl.classList.remove('open');
-        }
-    }
-}
-
-modal();
-
-
-// Disabled fake cover
-/*
-fake_cover.addEventListener('mousemove', () => {
-    fake_cover.classList.remove('visible');
-});*/
-
-
-
-
-
-function loadProgress(sPar) {
-
-    // Timer
-    let timer = 0;
-    let limit = 30000; //ms
-
-    timerEnd = limit / 10;
-    let blockWidth = 100 / timerEnd;
-    const progress = document.querySelectorAll("." + sPar + " .progress");
-
-    let countdown = setInterval(function () {
-        timer++;
-        for (var i = 0; i < progress.length; ++i) {
-            progress[i].style.width = timer * blockWidth + "%";
-
-            // setear a 0
-            if (document.querySelector('.pane').classList.contains('open')) {
-                clearInterval(countdown);
-            }
-
-        }
-        console.log(timer);
-        // document.getElementById("countdown").textContent = timer/100;
-        if (timer >= timerEnd) {
-            clearInterval(countdown);
-            document.getElementsByClassName('toggle')[0].click();
-        }
-    }, 10);
-
-}
-//miliseconds
-console.log('kionda');
-
-
-// Para cuando no hay modal al inicio
-const progress_direct = document.getElementsByClassName("progress-direct")[0];
-
-if (progress_direct) {
-    loadProgress('progress-direct');
-}
-//
 
 
 
@@ -291,8 +115,8 @@ if (button_ok) {
 
 /* Select option */
 
-/*const answer_select = document.querySelectorAll('.li-questions li');
-const next_select = document.getElementById('link-onboard');
+const answer_select = document.querySelectorAll('.pulse');
+const next_select = document.getElementById('link-next');
 
 if (answer_select) {
     answer_select.forEach(function (link) {
@@ -300,10 +124,10 @@ if (answer_select) {
         link.addEventListener('click', () => {
 
             [].forEach.call(answer_select, function (el) {
-                el.classList.remove('selected');
+                el.parentElement.classList.remove('selected');
             });
 
-            link.classList.add('selected');
+            link.parentElement.classList.add('selected');
 
             if (next_select) {
                 next_select.classList.add('selected');
@@ -311,4 +135,4 @@ if (answer_select) {
             }
         });
     });
-}*/
+}
