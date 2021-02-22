c9373ee5f190c76f54f7393fe6b9b190ab0d044e
jositux <jm.guaimas@gmail.com>
2021-02-22 16:49:14 -0300

    Update Alba Laberinto PMV

--- public/laberinto/js/laberinto.js
+++ public/laberinto/js/laberinto.js
@@ -24,17 +24,44 @@ function createTitle(pTitle) {
 }
 
 
+
+
+
 window.onload = function () {
+
 
     setTimeout(() => {
         //player.stop();
         document.querySelectorAll('.steal')[0].classList.add('black');
         loadPlayer(video.dataset.video, video.dataset.poster);
-        window.player.play();
+        //window.player.play();
+
+        //player.volume = 0;
+        //player.play();
+
+        document.querySelectorAll('.close')[0].classList.remove('hide');
+
+
+        var promise = player.play();
+
+        if (promise !== undefined) {
+            promise.then(_ => {
+                // Autoplay started!
+                console.log('lalallaa 1');
+            }).catch(error => {
+                console.log('lalallaa');
+                // Autoplay was prevented.
+                // Show a "Play" button so that user can start playback.
+            });
+        }
+
+
+
+
 
         // Set Time comments
-        comment_init = false;
-        comment_end = false;
+        //comment_init = false;
+        // comment_end = false;
         settime(time_comments);
 
         document.getElementsByClassName('steal_title')[0].classList.add('hide');
@@ -51,7 +78,11 @@ window.onload = function () {
 
     }, 3000);
 
+    //volumen_boton.click();
+
+
 };
+
 
 
 // Pane Slide
@@ -64,12 +95,16 @@ if (button_open) {
     button_open.forEach(function (link) {
 
         link.addEventListener('click', () => {
-            pane.classList.add('open');
+
 
             loadPlayer(video.dataset.video, video.dataset.poster);
-            comment_init = false;
-            comment_end = false;
-            settime(time_comments);
+            pane.classList.add('open');
+
+            if (video.hasAttribute("data-comments")) {
+                comment_init = false;
+                comment_end = false;
+                settime(time_comments);
+            }
             player.play();
 
         });
@@ -106,7 +141,7 @@ function loadPlayer(sURL, sPoster) {
     let source = 'https://streaming.rtvc.gov.co/RTVCPlay-vod/smil:' + sURL + '.smil/playlist.m3u8';
     const video = document.querySelector('video');
 
-    video.setAttribute('poster', sPoster);
+    //video.setAttribute('poster', sPoster);
 
 
     // For more options see: https://github.com/sampotts/plyr/#options
@@ -133,6 +168,11 @@ function loadPlayer(sURL, sPoster) {
         player.on('languagechange', () => {
             // Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
             //setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
+        });
+
+        player.on('ready', event => {
+            //delayedPlay();
+            //player.volume = 0.5;
         });
 
         player.on('play', function () {
@@ -166,6 +206,22 @@ function loadPlayer(sURL, sPoster) {
 
 
 
+function sleep(ms) {
+    return new Promise(
+        resolve => setTimeout(resolve, ms)
+    );
+}
+
+async function delayedPLay() {
+    console.log("Hello");
+    await sleep(2000);
+    volumen_boton.click();
+}
+
+//delayedPlay();
+
+
+
 
 
 /* Select option */
@@ -189,7 +245,9 @@ if (answer_select) {
                  next_select.href = link.dataset.rel;
              }*/
 
-            window.location.href = link.dataset.rel;;
+            console.log(link.dataset.rel);
+
+            window.location.href = link.dataset.rel;
 
         });
     });
